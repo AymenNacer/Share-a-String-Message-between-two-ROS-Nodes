8fb894394bd923c43c93e4f0c1d434ce9bcac909
AymenNacer <67188835+AymenNacer@users.noreply.github.com>
2020-07-07 22:27:07 +0300

    Share-a-String-Message-between-Two-ROS-Nodes

--- Aymen_ws/Aymen_ws/build/Aymen_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aymen/Documents/Aymen_ws/src/Aymen_package
-# Build directory: /home/aymen/Documents/Aymen_ws/build/Aymen_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Aymen_ws/Aymen_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aymen/Documents/Aymen_ws/src
-# Build directory: /home/aymen/Documents/Aymen_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("Aymen_package")
